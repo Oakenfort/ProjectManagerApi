ef2e4f4c6613b883421ed3d9d6e591eb7844a429
omar <omar.aboukher501@gmail.com>
2021-12-23 22:48:39 +0100

    initial commit

---
+++ src/main/java/com/projectManagerApi/projectManagerApi/ProjectManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.projectManagerApi.projectManagerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerApiApplication.class, args);
+	}
+
+}
